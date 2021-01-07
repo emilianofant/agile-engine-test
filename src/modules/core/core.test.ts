41f1e6cab2edc8efeb658b591bbaf2c195ddecba
Emiliano Fant <emilianofant@github.com>
2021-01-07 11:22:01 -0300

    feat: added request for auth and images

--- src/modules/core/core.test.ts
+++ src/modules/core/core.test.ts
@@ -1,3 +1,4 @@
+import { number } from 'yargs';
 import Core from './core';
 
 describe('Core class tests', () => {
@@ -5,5 +6,33 @@ describe('Core class tests', () => {
 
   test('Health check is fine', () => {
     expect(newCore.healthCheck()).toEqual('check');
+  });
+
+  test('Get Auth Bearer token', () => {
+    return newCore.getAuth().then((res) => {
+      expect(res.auth).toBe(true);
+    });
+  });
+
+  test('Get images from API', () => {
+    return newCore.getImages().then((res) => {
+      expect(res).toEqual({
+        pictures: expect.any(Array),
+        hasMore: expect.any(Boolean),
+        page: expect.any(Number),
+        pageCount: expect.any(Number),
+      });
+    });
+  });
+
+  test('Get images from API within a specific page 2', () => {
+    return newCore.getImages(2).then((res) => {
+      expect(res).toEqual({
+        pictures: expect.any(Array),
+        hasMore: expect.any(Boolean),
+        page: 2,
+        pageCount: expect.any(Number),
+      });
+    });
   });
 });
--- src/modules/core/core.ts
+++ src/modules/core/core.ts
@@ -3,8 +3,46 @@
  * and other stuff required for the app to run.
  */
 class Core {
+  apikey: string;
+  baseUrl: string;
+  authBearer: string | null;
+
+  constructor() {
+    // @todo: move to a .env
+    this.apikey = '23567b218376f79d9415';
+    this.baseUrl = 'http://interview.agileengine.com';
+    this.authBearer = null;
+  }
+
   healthCheck(): string {
     return 'check';
+  }
+
+  async getAuth() {
+    const body = { apiKey: this.apikey };
+    const res = await this.postData(`${this.baseUrl}/auth`, body);
+    if (res.auth) {
+      this.authBearer = res.token;
+    }
+    return res;
+  }
+
+  async getImages(page?: number) {
+    const url = `${this.baseUrl}/images${page ? '?page=' + page : ''}`;
+
+    return await fetch(url, {
+      headers: { Authorization: `Bearer ${this.authBearer}` },
+    }).then((res) => res.json());
+  }
+
+  async postData(url = '', data = {}): Promise<any> {
+    return await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    }).then((res) => res.json());
   }
 }
 
